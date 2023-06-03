284eb2fb777111d10461810497c3e77b055253e0
ken0901 <ken5260715@gmail.com>
2023-06-02 22:47:06 -0700

    REST API - DELETE Request method

--- src/main/java/com/ken/app/controller/TodoController.java
+++ src/main/java/com/ken/app/controller/TodoController.java
@@ -3,12 +3,12 @@ package com.ken.app.controller;
 import com.ken.app.entity.Todo;
 import com.ken.app.service.TodoHardcodeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class TodoController {
 
@@ -18,5 +18,14 @@ public class TodoController {
     @GetMapping("/users/{username}/todos")
     public List<Todo> getAllTodos(@PathVariable String username){
         return this.todoService.findAll();
+    }
+
+    @DeleteMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id){
+        Todo todo = todoService.deleteById(id);
+        if(todo!=null){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
