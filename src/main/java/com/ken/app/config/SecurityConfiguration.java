298b7789dd3df67b740526bf97d95c721ee21322
ken0901 <ken5260715@gmail.com>
2023-06-06 21:58:28 -0700

    Configure Spring Security to disable CSRF and enable OPTION Request

---
+++ src/main/java/com/ken/app/config/SecurityConfiguration.java
@@ -0,0 +1,51 @@
+package com.ken.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    //1: Response to preflight request doesn't pass access control check
+    //2: basic auth
+
+    // Spring Boot 2
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    /*
+    * this needs Spring Boot 3
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //1: Response to preflight request doesn't pass access control check
+        //2: basic auth
+        return
+                http
+                        .authorizeHttpRequests(
+                                auth ->
+                                        auth
+                                                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                                                .anyRequest().authenticated()
+                        )
+                        .httpBasic(Customizer.withDefaults())
+                        .sessionManagement(
+                                session -> session.sessionCreationPolicy
+                                        (SessionCreationPolicy.STATELESS))
+                        .csrf().disable()
+                        .build();
+    }*/
+
+}
